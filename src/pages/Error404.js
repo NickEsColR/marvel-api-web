da17a7d72caea819cf58fd6f9ca3c06e2b1500a0
NickEsColR <nicoesol0818@gmail.com>
2024-02-13 14:41:27 -0500

    style: add style to error page

--- src/pages/Error404.js
+++ src/pages/Error404.js
@@ -1,7 +1,15 @@
-import React from 'react'
+import React from "react";
 
 export const Error404 = () => {
   return (
-    <div>dentro de Error404</div>
-  )
-}
+    <div className="error-404">
+      <div className="error-404-content-text">
+        <h1>Error 404</h1>
+        <h2>Activando armadura... cargando programa</h2>
+        <h2>Jarvis no ha encontradado el protocolo buscado</h2>
+        <h2>Intenta volver a la página anterior o con los protocolos del menu</h2>
+        <h3>Ironman fuera</h3>
+      </div>
+    </div>
+  );
+};
